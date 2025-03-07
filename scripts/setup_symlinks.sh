1a4d84da3fbc7c636cd977528e2429bb8850cb3c
Andrei <81142468+kensi-rus@users.noreply.github.com>
2025-03-07 15:27:35 +0400

    Update setup_symlinks.sh
    Added zsh + ohmyzsh install with some nondefault plugins

--- scripts/setup_symlinks.sh
+++ scripts/setup_symlinks.sh
@@ -6,6 +6,22 @@ CONFIG_DIR="$HOME/.aux/configs"
 # Список файлов для переноса и создания симлинков
 FILES=(".tmux.conf" ".nanorc" ".zshrc")
 
+# Проверка наличия Zsh
+if ! command -v zsh &>/dev/null; then
+  echo "Zsh не установлен. Устанавливаем..."
+  sudo apt update && sudo apt install -y zsh || sudo yum install -y zsh
+else
+  echo "Zsh уже установлен."
+fi
+
+# Проверка наличия Oh My Zsh
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  echo "Oh My Zsh не найден. Устанавливаем..."
+  sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" --unattended
+else
+  echo "Oh My Zsh уже установлен."
+fi
+
 # Перенос файлов и создание символических ссылок
 for FILE in "${FILES[@]}"; do
   if [ -f "$HOME/$FILE" ] || [ -L "$HOME/$FILE" ]; then
@@ -21,4 +37,21 @@ for FILE in "${FILES[@]}"; do
   fi
 done
 
-echo "All done!"
+# Установка плагинов Oh My Zsh
+ZSH_CUSTOM=${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}
+PLUGINS=("zsh-autosuggestions" "zsh-syntax-highlighting")
+
+for PLUGIN in "${PLUGINS[@]}"; do
+  if [ ! -d "$ZSH_CUSTOM/plugins/$PLUGIN" ]; then
+    echo "Installing plugin: $PLUGIN..."
+    git clone "https://github.com/zsh-users/$PLUGIN" "$ZSH_CUSTOM/plugins/$PLUGIN"
+  else
+    echo "Plugin $PLUGIN already installed."
+  fi
+done
+
+# Обновление конфигурации
+echo "Переключение на Zsh..."
+chsh -s $(which zsh)
+
+echo "All done! Перезапустите терминал или выполните команду 'zsh' для входа в новую оболочку."
